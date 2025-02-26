2167f106d7ea2dca88ad594ed19a01a5c2067e05
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-02-26 10:43:21 +0100

    Refactor Quiz component by extracting Question into a separate component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,42 +1,32 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import QUESTIONS from "../questions.js";
-import quizCompletedImg from '../assets/quiz-complete.png'
+import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
+
     const activeQuestionIndex = userAnswers.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswer) => {
-            return [...prevUserAnswer, selectedAnswer];
+            return [...prevUserAnswer, selectedAnswer?.answer || selectedAnswer];
         });
-    }
+    }, []);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
-        return (
-            <div id='summary'>
-                <img src={quizCompletedImg} alt='quizCompleted'/>
-                <h2>Quiz Completed!</h2>
-            </div>
-        )
+        return <Summary userAnswers={userAnswers}/>
     }
-
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div id='quiz'>
-            <div id='question'>
-                <h2 className="Quiz">{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id='answers'>
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className='answer'>
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Question index={activeQuestionIndex}
+                      key={activeQuestionIndex}
+                      onSelectAnswer={handleSelectAnswer}
+                      onSkipAnswer={handleSkipAnswer}/>
         </div>
     )
 }
